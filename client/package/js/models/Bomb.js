e88c7fb20a21eb8155f607cfbb4a563228a09a1e
eldwin <eldwin_@hotmail.com>
2012-10-27 23:00:44 +0800

    copied models to client. will work on there until partial offline game is playable

---
+++ client/package/js/models/Bomb.js
@@ -0,0 +1,51 @@
+//// IMPORTS
+
+
+
+function Bomb( x, y, range, owner )
+{
+
+//// PRIVATE VARIABLES
+  this._x = x;          // int - x grid bomb is on
+  this._y = y;          // int - y grid bomb is on
+  this._range = range;  // int - range of explosion [ > 1 ]
+  this._owner = owner;  // int - player id who dropped bomb
+}
+
+
+//// PUBLIC FUNCTIONS
+
+Bomb.prototype.GetX = function()
+{
+  return this._x;
+}
+
+Bomb.prototype.GetY = function()
+{
+  return this._y;
+}
+
+Bomb.prototype.GetOwner = function()
+{
+  return this._owner;
+}
+
+Bomb.prototype.GetRange = function()
+{
+  return this._range;
+}
+
+// representation
+Bomb.prototype.Serialize = function()
+{
+  return  {
+    x: this._x,
+    y: this._y,
+    range: this._range,
+    //owner: this._owner
+  };
+}
+
+
+//// EXPORTS
+module.exports = Bomb;
---
+++ client/package/js/models/Player.js
@@ -0,0 +1,149 @@
+//// IMPORTS
+var Bomb = require( './Bomb' );
+
+
+function Player( socket )
+{
+//// PRIVATE VARIABLES
+  this._id;	      		    // int - identifier - [0 to Room.MAX - 1]
+  this._socket = socket;	// socket
+
+  
+  // this._x;                // int - player's x co-ordinates in pixels
+  // this._y;                // int - player's y co-ordinates in pixels
+  // this._speed;            // int - movement speed
+  // this._color;            // Player.color - player avatar color
+  // this._bombs;            // Bomb[] - bombs currently active in game map
+  // this._bomb_range;       // int - number of grids bomb explodes
+  // this._bomb_limit;       // int - number of bombs user can use at once
+  // this._powerups;         // Powerup[] - powerups player has - starts with none
+  // this._ability_kickbomb; // bool - whether player has ability to kick bombs
+  this.Reset();
+}
+
+
+//// CONSTANTS
+Player.Color =
+{
+  BLUE: 0,
+  GREEN: 1,
+  RED: 2,
+  PINK: 3,
+  NONE: 4,
+};
+
+Player.Default =
+{
+  SPEED: 1,
+  BOMBS: [],
+  POWERUPS: [],
+  BOMB_RANGE: 1,
+  BOMB_LIMIT: 1,
+  ABILITY_KICKBOMB: false,
+  COLOR: Player.Color.NONE,
+};
+
+
+//// PUBLIC FUNCTIONS
+
+Player.prototype.GetSocket = function()
+{
+  return this._socket;
+};
+
+Player.prototype.GetSpeed = function()
+{
+  return this._speed;
+}
+
+Player.prototype.SetSpeed = function( speed )
+{
+  this._speed = speed;
+}
+
+Player.prototype.GetBombRange = function()
+{
+  return this._bomb_range;
+}
+
+Player.prototype.SetBombRange = function( range )
+{
+  this._bomb_range = range;
+}
+
+Player.prototype.GetBombLimit = function()
+{
+  return this._bomb_limit;
+}
+
+Player.prototype.SetBombLimit = function( limit )
+{
+  this._bomb_limit = limit;
+}
+
+Player.prototype.GetAbilityKickBomb = function()
+{
+  return this._ability_kickbomb;
+}
+
+Player.prototype.SetAbilityKickBomb = function( canKickBomb )
+{
+  this._ability_kickbomb = canKickBomb;
+}
+
+
+// pick and apply powerup
+Player.prototype.PickPowerup = function( powerup )
+{
+  this._powerups.push( powerup.GetType() );
+  powerup.ApplyEffect( this );
+}
+
+// drop all powerups and return a list to game
+Player.prototype.DropAllPowerups = function()
+{
+  var temp = this._powerups;
+  this._powerups = Player.Default.POWERUPS.slice();
+  return temp;
+}
+
+// called when player gets killed, resets all stats except color
+Player.prototype.Reset = function()
+{
+  this._x = 0;
+  this._y = 0;
+  this._color = Player.Default.COLOR;
+  this._speed = Player.Default.SPEED;
+  this._bombs = Player.Default.BOMBS.slice();
+  this._bomb_range = Player.Default.BOMB_RANGE;
+  this._bomb_limit = Player.Default.BOMB_LIMIT;
+  this._powerups = Player.Default.POWERUPS.slice();
+  this._ability_kickbomb = Player.Default.ABILITY_KICKBOMB;
+}
+
+// drop bomb on nearest grid
+// returns Bomb object
+Player.prototype.Bomb = function()
+{
+  // var bomb = new Bomb( 0, 0, this._bomb_range );
+  // this._bombs.push( bomb );
+  // return bomb;
+}
+
+// representation
+// only to be called when informing other players of who is in waiting room
+Player.prototype.Serialize = function()
+{
+  return {
+    id: this._id,
+    color: this._color,
+    speed: this._speed,
+    range: this._bomb_range,
+    limit: this._bomb_limit,
+    powerups: this._powerups,
+  };
+}
+
+
+//// EXPORTS
+module.exports = Player;
