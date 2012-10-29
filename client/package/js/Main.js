ed647cbd47440e1fe088e22afd9345cf2eea3800
neonaleon <neon.aleon@hotmail.com>
2012-10-29 15:06:10 +0800

    completed joystick, updated buttons

--- client/package/js/Main.js
+++ client/package/js/Main.js
@@ -1,4 +1,4 @@
-// main.js
+Math.PI*3/4// main.js
 // @author Leon Ho
 
 window.onload = function()
@@ -6,7 +6,9 @@ window.onload = function()
 	Crafty.init(Properties.DEVICE_WIDTH, Properties.DEVICE_HEIGHT);
 	Crafty.background("#FFFFFF");
 	
-	SceneManager.RunWithScene(SceneDefinitions.GameScene); //SceneManager.RunWithScene(SceneDefinitions.SplashScene);
+	//SceneManager.RunWithScene(SceneDefinitions.WaitingRoomScene);
+	SceneManager.RunWithScene(SceneDefinitions.GameScene);
+	//SceneManager.RunWithScene(SceneDefinitions.SplashScene);
 };
 
 
--- client/package/js/entities/base/GUI.js
+++ client/package/js/entities/base/GUI.js
@@ -3,79 +3,71 @@
  * @author: Leon Ho
  * This file contains factory methods for generating GUI entities
  */
-
-
-
 var GUI = {};
+
+Crafty.c('Button', {
+	init: function(){
+		this.requires("Mouse");
+		this.isDown = false;
+		this._onButtonDownFunc = undefined,
+		this._onButtonOutFunc = undefined,
+		this._onButtonUpFunc = undefined,
+			
+		this.bind("MouseDown", function()
+		{
+			if (!this.isDown) 
+			{
+				this.isDown = true;
+				this._onButtonDownFunc();
+			}
+		});
+		this.bind("MouseOut", function(){
+			this.isDown = false;
+			this._onButtonOutFunc();
+		});
+		this.bind("MouseUp", function(){
+			if (this.isDown)
+			{
+				this.isDown = false;
+				this._onButtonUpFunc();
+			}
+		});
+		return this;
+	},
+	onButtonDown: function (func) {this._onButtonDownFunc = func; return this;},
+	onButtonOut: function (func) {this._onButtonOutFunc = func; return this;},
+	onButtonUp: function (func) {this._onButtonUpFunc = func; return this;},
+})
 
 // Creates a button with buttonText, which invokes handler(this) when clicked
 GUI.Button = function(buttonText, handler)
 {
-	return Crafty.e(Properties.RENDERER + ", 2D, Color, Text, Mouse")
+	return Crafty.e(Properties.RENDERER + ", 2D, Color, Text, Button")
 				.setName("button_" + buttonText)
-				.attr({	isDown:false, w:GUIDefinitions.BUTTON_WIDTH, h:GUIDefinitions.BUTTON_HEIGHT })
+				.attr({ w:GUIDefinitions.BUTTON_WIDTH, h:GUIDefinitions.BUTTON_HEIGHT })
 				.color(GUIDefinitions.BUTTON_UPCOLOR)
 				.text(buttonText)
-				.bind("MouseDown", function()
-				{
-					if (!this.isDown) 
-					{
-						this.isDown = true;
-						this.color(GUIDefinitions.BUTTON_DOWNCOLOR);
-					}
-				})
-				.bind("MouseOut", function(){
-					this.isDown = false;
-					this.color(GUIDefinitions.BUTTON_UPCOLOR);
-				})
-				.bind("MouseUp", function(){
-					if (this.isDown)
-					{
-						this.isDown = false;
-						this.color(GUIDefinitions.BUTTON_UPCOLOR);
-						handler(this);
-					}
-				});
+				.onButtonDown(function(){ this.color(GUIDefinitions.BUTTON_DOWNCOLOR); })
+				.onButtonOut(function(){ this.color(GUIDefinitions.BUTTON_UPCOLOR); })
+				.onButtonUp(function(){ this.color(GUIDefinitions.BUTTON_UPCOLOR); handler(this);});
 };
 
 GUI.ACTION_BUTTON_A = 'A';
 GUI.ACTION_BUTTON_B = 'B';
-GUI.ActionButton = function(enumButton, handler)
-{
-	return Crafty.e(Properties.RENDERER + ", 2D, Color, ActionButton")
-				.setName("actionButton_" + enumButton);
-};
 
-GUI.TexturedButton = function ()
+GUI.ActionButton = function(button)
 {
-	
-};
+	return Crafty.e(Properties.RENDERER + ", 2D, Button"); // add a action button sprite!
+}
 
 // Creates a joystick at posx, posy which moves the entity obj with speed
 GUI.Joystick = function (posx, posy, obj, speed)
 {
-	/*
-	// INCOMPLETE, NEED TO SET X, Y, W, H, according to image we are going to use
-	var stick = Crafty.e(Properties.RENDERER + ", 2D, Image").attr({
-                x: posx+5,
-                y: posy+5,
-                w: 50,
-                h: 50,
-                z: 1              
-    }).image("http://cdn1.iconfinder.com/data/icons/function_icon_set/circle_green.png");
-
-    var track = Crafty.e(Properties.RENDERER + ", 2D, Image, Joystick, Text").attr({
-        x: posx,
-        y: posy,
-        w: 64,
-        h: 64       
-    }).image("http://cdn1.iconfinder.com/data/icons/softwaredemo/PNG/64x64/Circle_Grey.png")
-    .joystick(stick, {mouseSupport: true, range: 20})
-    .attach(stick);
-    */
    var joystick = Crafty.e('Controller').attr({x:posx, y:posy});
     //obj.addComponent("Keyboard, Multiway").multiway(speed, {UP_ARROW: -90, DOWN_ARROW: 90, RIGHT_ARROW: 0, LEFT_ARROW: 180});
-   	obj.addComponent("Controllable").setSpeed(speed);
+   	obj.addComponent("Controllable").controllable(speed, undefined, undefined);
+   	
+   	return joystick;
 }
 
 GUI.RoomButton = function(data)
