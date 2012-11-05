5bd44b7dbbfa010cde3a77ae6f7f92c500b790f3
Natalie <natalie.tay@gmail.com>
2012-11-05 16:42:43 +0800

    after meeting
    nat todo.

--- client/package/js/definitions/SpriteDefinitions.js
+++ client/package/js/definitions/SpriteDefinitions.js
@@ -44,7 +44,8 @@ for (var i = 0; i < SpriteDefinitions.COLORS.length; i++)
 			elements[color + 'dragon'] = [5, i];
 			elements[color + 'egg'] = [10, i];
 			// temporary
-			elements['fire'] = [10, 2];
+			elements['fire'] = [3, 4];
+			elements['anim_fire'] = [[3, 4], [5,4]];
 			return elements;
 		})(),
 		'anim_walk_up':[[0, i], [1, i]],
