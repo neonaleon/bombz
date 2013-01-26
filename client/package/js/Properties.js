e40d2c65c45f97f98daf2003cf244b170eeafd41
neonaleon <neon.aleon@hotmail.com>
2013-01-26 11:03:20 +0800

    update properties!!

--- client/package/js/Properties.js
+++ client/package/js/Properties.js
@@ -10,10 +10,11 @@ var Properties =
 	RENDERER : "DOM", // or Canvas NOTE: Canvas renderer will be a breaking change
 	
 	// server properties
-	MASTERSERVER_IP: "192.168.1.100",
+	//MASTERSERVER_IP: "192.168.1.100",
 	//MASTERSERVER_IP: "http://localhost",
 	//MASTERSERVER_IP: "http://ec2-184-73-92-233.compute-1.amazonaws.com",
-	MASTERSERVER_PORT: 8000,
+	MASTERSERVER_IP: "http://bombz.herokuapp.com/"
+	MASTERSERVER_PORT: process.env.PORT || 8000,
 	
 	// game properties
 	SPLASH_DURATION: 2000,
